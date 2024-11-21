d90889f202c0c4b0b42f7d3c826972152415bac9
Marcelle Castro <131879933+marczlle@users.noreply.github.com>
2024-11-21 19:38:34 -0300

    Create Retangulo.java

---
+++ questao2/Retangulo.java
@@ -0,0 +1,24 @@
+class Retangulo implements ObjetoGeometrico {
+    private double width;
+    private double length;
+
+    public Retangulo(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[width=" + width + ", length=" + length + "]";
+    }
+}
