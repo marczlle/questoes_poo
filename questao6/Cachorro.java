7e3e0cbe2d951516c9635affb0e1fb72a97f3a87
Marcelle Castro <131879933+marczlle@users.noreply.github.com>
2024-11-21 18:48:04 -0300

    Create Cachorro.java

---
+++ questao6/Cachorro.java
@@ -0,0 +1,15 @@
+public class Cachorro extends Animal {
+
+    public Cachorro(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void cumprimentar() {
+        System.out.println("Au au");
+    }
+  
+    public void saudar(Cachorro outro) {
+        System.out.println("Auuuuuu");
+    }
+}
